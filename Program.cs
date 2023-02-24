2feea3739ae8ccbc2f4db07cdb16c34ab4cf2c45
Austin Fogle <A-Fogle1994@outlook.com>
2023-02-24 16:09:53 -0500

    New Web API

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
-using team_status_undefined_backend.Migrations;
-using team_status_undefined_backend.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSqlite<BarberDbContext>("Data Source=team_status_undefined_backend.db");
-builder.Services.AddScoped<IBarberRepository, BarberRepository>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
