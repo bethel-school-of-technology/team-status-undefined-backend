86aa62187eb7c603ddb2144efea5ae14b21725cc
Austin Fogle <A-Fogle1994@outlook.com>
2023-02-24 15:07:47 -0500

    New Web API

--- Program.cs
+++ Program.cs
@@ -1,80 +1,20 @@
-using team_status_undefined_backend.Migrations;
-using team_status_undefined_backend.Repositories;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "team_status_undefinedAPI", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
-        In = ParameterLocation.Header, 
-        Description = "Please insert JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey 
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-    { 
-        new OpenApiSecurityScheme 
-        { 
-        Reference = new OpenApiReference 
-        { 
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer" 
-        } 
-        },
-        new string[] { } 
-        } 
-    });
-});
+// Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSqlite<BarberDbContext>("Data Source=team_status_undefined_backend.db");
-builder.Services.AddScoped<IBarberRepository, BarberRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-var secretKey = builder.Configuration["TokenSecret"];
-
-builder.Services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(cfg =>
-{
-    cfg.RequireHttpsMetadata = true;
-    cfg.SaveToken = true;
-    cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
-        ValidateAudience = false,
-        ValidateIssuer = false,
-        ValidateLifetime = false,
-        RequireExpirationTime = false,
-        ClockSkew = TimeSpan.Zero,
-        ValidateIssuerSigningKey = true
-    };
-});
 
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors(builder => builder
-    .WithOrigins("http://localhost:4200", "http://localhost:3000")
-    .AllowAnyHeader()
-    .AllowAnyMethod());
 
 app.UseHttpsRedirection();
 
--- Repositories/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using team_status_undefined_backend.Models;
-
-namespace team_status_undefined_backend.Repositories;
-
-public interface IAuthService
-{
-    SignIn CreateSignIn(SignIn signin);
-    string LogIn (string email, string password);
-}
